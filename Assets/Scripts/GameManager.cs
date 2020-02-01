22bfefb66830412ac41cb5030eed368aa9907ca6
RyanStewart <ryan92130@gmail.com>
2020-01-31 22:23:44 -0500

    Added game manager for evaulating game state.  Transitions between build, evaluate, success and failure.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider outOfBoundsCatcher;
+
+    [SerializeField]
+    private BoxCollider heightRequirementCollider;
+
+    [SerializeField]
+    private Stackable[] stackables;
+
+    enum GameStates { BUILD, EVALUATE, SUCCESS, FAILURE };
+    [SerializeField]
+    private GameStates currentState = GameStates.BUILD;
+
+    [SerializeField]
+    private float maxTimeSeconds = 5f;
+
+    [SerializeField]
+    private float currentTimerSeconds = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stackables = GameObject.FindObjectsOfType<Stackable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case GameStates.BUILD:
+                // Check if any blocks are out of bounds or currently being held
+                bool isReadyToEvaluate = true;
+                foreach (Stackable stackable in stackables)
+                {
+                    if (stackable.CheckIfGrabbed())
+                    {
+                        isReadyToEvaluate = false;
+                        break;
+                    }
+                    if (!stackable.CheckInBounds())
+                    {
+                        isReadyToEvaluate = false;
+                        break;
+                    }
+                }
+                if (!isReadyToEvaluate)
+                {
+                    break;
+                }
+                // Otherwise its time to evaluate if the building will actually hold up
+                ToEvaluateState();
+                break;
+            case GameStates.EVALUATE:
+                currentTimerSeconds -= Time.deltaTime;
+                if (currentTimerSeconds <= 0f)
+                {
+                    ToSuccessState();
+                    break;
+                }
+                // Check if any blocks have fallen out of bounds or if the player grabbed one
+                foreach (Stackable stackable in stackables)
+                {
+                    if (stackable.CheckIfGrabbed())
+                    {
+                        ToBuildState();
+                        break;
+                    } 
+                    if (!stackable.CheckInBounds())
+                    {
+                        ToFailedState();
+                        break;
+                    }
+                }
+                break;
+        }
+    }
+
+    void ToBuildState()
+    {
+        currentTimerSeconds = maxTimeSeconds;
+        currentState = GameStates.BUILD;
+    }
+
+    void ToEvaluateState()
+    {
+        currentTimerSeconds = maxTimeSeconds;
+        currentState = GameStates.EVALUATE;
+    }
+
+    void ToSuccessState()
+    {
+        foreach (Stackable stackable in stackables)
+        {
+            stackable.MakeStatic();
+        }
+        currentTimerSeconds = 0f;
+        currentState = GameStates.SUCCESS;
+    }
+
+    void ToFailedState()
+    {
+        currentState = GameStates.FAILURE;
+    }
+}
---
+++ Assets/Scripts/Stackable.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stackable : MonoBehaviour
+{
+    [SerializeField]
+    private bool inBounds = false;
+
+    private Rigidbody rigidbody;
+    private Collider collider;
+    private OVRGrabbable grabbable;
+    private OculusSampleFramework.DistanceGrabbable distanceGrabbable;
+
+    private Vector3 startingPosition;
+    private Quaternion startingRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+        collider = this.GetComponent<Collider>();
+        grabbable = this.GetComponent<OVRGrabbable>();
+        distanceGrabbable = this.GetComponent<OculusSampleFramework.DistanceGrabbable>();
+
+        startingPosition = this.transform.position;
+        startingRotation = this.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<OutOfBoundsCatcher>())
+        {
+            Debug.Log("Enter collider");
+            inBounds = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<OutOfBoundsCatcher>())
+        {
+            Debug.Log("Exit collider");
+            inBounds = true;
+        }
+    }
+
+    // Surface if the item is currently held
+    public bool CheckIfGrabbed() => grabbable.isGrabbed;
+
+    // Surface if the item is in bounds
+    public bool CheckInBounds() => inBounds;
+
+    // Make this grabbable unmovible and not grabbable
+    public void MakeStatic()
+    {
+        rigidbody.isKinematic = true;
+        collider.enabled = false;
+        distanceGrabbable.enabled = false;
+    }
+
+    // Make this object moveable and grabbable
+    public void MakeInteractible()
+    {
+        rigidbody.isKinematic = false;
+        collider.enabled = true;
+        distanceGrabbable.enabled = true;
+    }
+}
